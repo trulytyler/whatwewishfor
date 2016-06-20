ca0ada6271f1f13c681451be91e9e6f2f9a0d7c4
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-19 22:23:13 -0700

    hh

--- configuration/config.js
+++ configuration/config.js
@@ -6,7 +6,7 @@ module.exports={
 
 	"twitter_api_key" 		: 			"yAWKY2ymg54UsQR766Rw8IWCl",
 	"twitter_api_secret"	:				"9XNczKMzVpsYRocMV8TDbROQ6Zg5cSuyZXGxijowmbrrNnBjLA",
-	"callback_url"				:				"https://whatwewishfor.herokuapp.com/",
+	"callback_url"				:				"https://whatwewishfor.herokuapp.com/home.html",
 	"use_database"				:				"false",
 	"host"								:				"localhost",
 	"username"						:				"root",
