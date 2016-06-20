82f4f04c5c87d0e69e9d0ff61d472d3bb97241eb
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-19 23:50:22 -0700

    config

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"twitter_api_key" 		: 			"yAWKY2ymg54UsQR766Rw8IWCl",
+	"twitter_api_secret"	:				"9XNczKMzVpsYRocMV8TDbROQ6Zg5cSuyZXGxijowmbrrNnBjLA",
+	"callback_url"				:				"https://whatwewishfor.herokuapp.com/",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				"twitter_login"
+}
