1bc37302f6b1f70f52d64bdc11fd0d665a029cfe
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-22 19:16:34 -0700

    hmm

--- js/mainjs-works.js
+++ js/mainjs-works.js
@@ -23,55 +23,42 @@ var config = {
 });
 ///my code is above
 
-//New background onlick
-$bgndVideo = $('#bgndVideo');
-
-
- $( "#submitWish" ).click(function() {
-  alert( "Handler for .click() called." );
- $bgndVideo.attr( "data-property" ).append('"{videoURL:' + 'dOes_0f0knw' + ',containment:' + '#video-wrapper' + ',autoPlay:true, mute:true, startAt:0, opacity:1}') 
-
-});
-
- // $( "#bgndVideo" ).append('hello');
-
-
 //test
-// $(document).ready(function () {
+$(document).ready(function () {
      
-//     createButton();
-//    document.getElementById('dbutton').addEventListener("click",    createButton);
+    createButton();
+   document.getElementById('dbutton').addEventListener("click",    createButton);
 
-//     function createButton() {
+    function createButton() {
 
-//         // Remove Whatever is in the tweeetbox div if theres somethign 
-//         //there to avoid adding multiple tweetbuttons
+        // Remove Whatever is in the tweeetbox div if theres somethign 
+        //there to avoid adding multiple tweetbuttons
 
         
-//         var elem = document.getElementById('twitterbutton');
-//         if (elem != null) {
-//             elem.parentNode.removeChild(elem);
-//         }
+        var elem = document.getElementById('twitterbutton');
+        if (elem != null) {
+            elem.parentNode.removeChild(elem);
+        }
 
-//         // Create a New Tweet Element
-//         msg  =  document.getElementById('msg').value;
-//         var link = document.createElement('a');
-//         link.setAttribute('href', 'https://twitter.com/share');
-//         link.setAttribute('class', 'twitter-share-button');
-//         link.setAttribute('style', 'margin-top:5px;');
-//         link.setAttribute('id', 'twitterbutton');
-//         link.setAttribute("data-text", "" + msg + "");
-//         link.setAttribute("data-via", "denvycom");
-//         link.setAttribute("data-size", "large");
+        // Create a New Tweet Element
+        msg  =  document.getElementById('msg').value;
+        var link = document.createElement('a');
+        link.setAttribute('href', 'https://twitter.com/share');
+        link.setAttribute('class', 'twitter-share-button');
+        link.setAttribute('style', 'margin-top:5px;');
+        link.setAttribute('id', 'twitterbutton');
+        link.setAttribute("data-text", "" + msg + "");
+        link.setAttribute("data-via", "denvycom");
+        link.setAttribute("data-size", "large");
 
-//        // Put it inside the twtbox div
-//         tweetdiv  =  document.getElementById('twtbox');
-//         tweetdiv.appendChild(link);
+       // Put it inside the twtbox div
+        tweetdiv  =  document.getElementById('twtbox');
+        tweetdiv.appendChild(link);
 
-//         twttr.widgets.load();
-//     }
-// });
-// //end of test
+        twttr.widgets.load();
+    }
+});
+//end of test
 
 $(document).ready(function(){
 
--- js/video-bg.js
+++ js/video-bg.js
@@ -2,3 +2,13 @@ $(document).ready(function(){
 	//Video Background
 	$(".player").mb_YTPlayer();
 });
+
+$wishButton = $('#submitWish');
+$bgndVideo = $('#bgndVideo');
+
+$wishButton.on('click', function (event) {
+event.preventDefault();
+
+$bgndVideo.append('hello');
+
+});
