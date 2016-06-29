eba20c11b6199d7b6d088b9e3287da56428698b0
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-29 11:08:04 -0700

    yay oauth is kinda working

--- js/main.js
+++ js/main.js
@@ -31,26 +31,26 @@ firebase.auth().signInWithRedirect(provider).then(function(result) {
   // ...
 });
 //get info
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-    // You can use these server side with your app's credentials to access the Twitter API.
-    var token = result.credential.accessToken;
-    var secret = result.credential.secret;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+// firebase.auth().getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+//     // You can use these server side with your app's credentials to access the Twitter API.
+//     var token = result.credential.accessToken;
+//     var secret = result.credential.secret;
+//     // ...
+//   }
+//   // The signed-in user info.
+//   var user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
 });
 
 //functionality
