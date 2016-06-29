7c43d19c1ecd9f64f4da3c8202cde149168c38c4
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-28 21:17:42 -0700

    hello

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 var myFirebaseRef = new Firebase("https://whatwewishfor-cc766.firebaseio.com/");
-
-var config = {
+  
+  var config = {
     apiKey: "AIzaSyDukY4vwsg3rqecCtDVkiPKB_-ntM_M5KY",
     authDomain: "whatwewishfor-cc766.firebaseapp.com",
     databaseURL: "https://whatwewishfor-cc766.firebaseio.com",
@@ -9,7 +9,7 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  $('#wishwell').submit(function(event){
+  $('#submitWish').submit(function(event){
     event.preventDefault();
     var wish = $('#wishes').val();
     var wishRef = myFirebaseRef.child('wishes');
@@ -21,57 +21,60 @@ var config = {
 
   })
 });
+
+
+
 ///my code is above
 
-//New background onlick
-$bgColor = $('#home');
+// //New background onlick
+// $bgColor = $('#home');
 
- $( "#submitWish" ).click(function() {
-   $bgColor.css({"background": "url(https://media.giphy.com/media/IJTCcELAbVgHK/giphy-tumblr.gif)", "background-size" : "100% 120%"})
-});
+//  $( "#submitWish" ).click(function() {
+//    $bgColor.css({"background": "url(https://media.giphy.com/media/IJTCcELAbVgHK/giphy-tumblr.gif)", "background-size" : "100% 120%"})
+// });
 
-//share link (open in a new window?)
-$shareButton = $('a#sharebutton')
-$ogLink = 'http://twitter.com/share?text=This%20is%20so%20easy%20%23whatwewishfor', 'window name', 'width=500, height=475'
-$wishButton = $('#submitWish')
-$wishes = $('#wishes')
+// //share link (open in a new window?)
+// $shareButton = $('a#sharebutton')
+// $ogLink = 'http://twitter.com/share?text=This%20is%20so%20easy%20%23whatwewishfor', 'window name', 'width=500, height=475'
+// $wishButton = $('#submitWish')
+// $wishes = $('#wishes')
 
-$shareButton.click(function(){
-  window.open($ogLink);
-  return true;
-});
-//on wish click update oglink with user input
-
-
-
-//input goes in to
-//text attricbute of share twitter 
-//new window opens
+// $shareButton.click(function(){
+//   window.open($ogLink);
+//   return true;
+// });
+// //on wish click update oglink with user input
 
 
 
+// //input goes in to
+// //text attricbute of share twitter 
+// //new window opens
 
-$wishButton = $('#submitWish')
 
-$wishButton.on('click', function (event) {
-event.preventDefault();
-function wish() {
-$.ajax({
-  url: 'https://omdbapi.com/?s=titanic',
-  type: 'GET',
-  dataType: 'json',
-  success: function info(response) {
-    console.log('yay it worked');
-    var titanic = (response.Year);
-    console.log('http://omdbapi.com/?s=titanic');
-        console.log(titanic);
-//run your code here
-//this is the message when its wrong
-}
-});
-}
 
-wish();
-});
 
-  
+// $wishButton = $('#submitWish')
+
+// $wishButton.on('click', function (event) {
+// event.preventDefault();
+// function wish() {
+// $.ajax({
+//   url: 'https://omdbapi.com/?s=titanic',
+//   type: 'GET',
+//   dataType: 'json',
+//   success: function info(response) {
+//     console.log('yay it worked');
+//     var titanic = (response.Year);
+//     console.log('http://omdbapi.com/?s=titanic');
+//         console.log(titanic);
+// //run your code here
+// //this is the message when its wrong
+// }
+// });
+// }
+
+// wish();
+// });
+
+//   
