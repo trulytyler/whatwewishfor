9cd702af97bb53737699ad9182374b692fdf04f9
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-06-29 20:53:14 -0700

    lll

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
 
 
-// user.firebase_data = {
-//   user_id: new Buffer(user.email).toString('base64'),
-//   company: !user.isSocial ? context.connection.replace(/\./g, '-') : null,
-//   foo: 'bar'
-// };
-// });
+//init firebase app 
+// Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyDukY4vwsg3rqecCtDVkiPKB_-ntM_M5KY",
+  //   authDomain: "whatwewishfor-cc766.firebaseapp.com",
+  //   databaseURL: "https://whatwewishfor-cc766.firebaseio.com",
+  //   storageBucket: "whatwewishfor-cc766.appspot.com",
+  // };
+
+  // firebase.initializeApp(config);
 
 
-// //oauth with firebase
-// var provider = new firebase.auth.TwitterAuthProvider();
-
-// firebase.auth().signInWithRedirect(provider).then(function(result) {
-//   // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-//   // You can use these server side with your app's credentials to access the Twitter API.
-//   var token = result.credential.accessToken;
-//   var secret = result.credential.secret;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-// //get info
-// firebase.auth().getRedirectResult().then(function(result) {
-//   if (result.credential) {
-//     // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-//     // You can use these server side with your app's credentials to access the Twitter API.
-//     var token = result.credential.accessToken;
-//     var secret = result.credential.secret;
-//     // ...
-//   }
-//   // The signed-in user info.
-//   var user = result.user;
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
 
 
 //functionality
-$(document).ready(function() {
-  var config = {
-    apiKey: "AIzaSyDukY4vwsg3rqecCtDVkiPKB_-ntM_M5KY",
-    authDomain: "whatwewishfor-cc766.firebaseapp.com",
-    databaseURL: "https://whatwewishfor-cc766.firebaseio.com",
-    storageBucket: "whatwewishfor-cc766.appspot.com",
-  };
-  firebase.initializeApp(config);
+ $(document).ready(function() {
   // create object instance of my Firebase database
   var myDBReference = new Firebase('https://whatwewishfor-cc766.firebaseio.com/');
 
@@ -96,7 +51,7 @@ $(document).ready(function() {
       $templateHTML.click(function() {
         var messageId = $(this).data('id');
         updateMessage(messageId);
-      });
+      })
       $('#messages-list').append($templateHTML);
     });
   });
@@ -118,7 +73,16 @@ $(document).ready(function() {
     messageReference.remove();
   }
 
+})
+///my code is below
+
+//New background onlick
+$bgColor = $('#home');
+
+ $("#submitWish").click(function() {
+   $bgColor.css({"background": "url(https://media.giphy.com/media/IJTCcELAbVgHK/giphy-tumblr.gif)", "background-size" : "100% 120%"});
 });
+
 
 
 ///my code is below
